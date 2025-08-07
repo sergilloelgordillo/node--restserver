9ddcd7902564e6ac865e8c380d9b565f167589b9
sergilloelgordillo <sergiodeharo23@gmail.com>
2025-08-07 13:22:23 -0600

    default port updated

--- models/server.js
+++ models/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 class Server{
     constructor(){
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
         this.usuariosPath = '/api/usuarios'//el endpoint que tu quieras ponerle
 
         //middlewares anadir funcionalidad a mi webserver
